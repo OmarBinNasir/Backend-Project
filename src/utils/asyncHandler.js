d5b7b09c5f8a21976f6c65e8f8aac985a4fe6ec0
KaiLegend56 <kaininja567@gmail.com>
2024-07-06 10:56:54 +0530

    installed cookieParser and CORS, asyncHandler, handle api error and response

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+    }
+}
+
+// const asyncHandler=()=>{}
+// const asyncHandler=(func)=>{ ()=>{} }
+// const asyncHandler=(func)=> async ()={ await func();}
+
+export {asyncHandler}
+
+
+// const asyncHandler=(fun)=> async (req,res,next)=>{
+//     try{
+//         await fun(req,res,next);
+
+//     }
+//     catch(error){
+//             res.status(error.code||500).json({
+//                 sucess:false,
+//                 message:error.message
+//             })
+//         }
+// }
+ //try catch method
